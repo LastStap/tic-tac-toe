b285693f7cac6af880d12bc3ba2ba2a08450487b
Daniil Dumshenko <dumshenko2003@gmail.com>
2024-10-24 12:10:05 +0200

    created field(Board)

---
+++ src/main/java/dumshenko/daniil/tic_tac_toe/App.java
@@ -0,0 +1,9 @@
+package dumshenko.daniil.tic_tac_toe;
+
+import static dumshenko.daniil.tic_tac_toe.Board.printBoard;
+
+public class App {
+    public static void main(String[] args) {
+        printBoard();
+    }
+}
